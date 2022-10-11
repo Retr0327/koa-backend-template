c7ab4892bea7cad1319521bd95ba70311a91339e
Retr0327 <philcoke35@gmail.com>
2022-10-11 17:35:27 +0800

    Add morgan

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,31 +1,17 @@
 import Koa from 'koa';
 import router from '@routes';
-import cors from '@koa/cors';
 import koaEtag from 'koa-etag';
-import morgan from 'koa-morgan';
-import { corsConfig } from '@configs';
+import cors from '@middlewares/cors';
 import bodyParser from 'koa-bodyparser';
-import customDevFormat from '@utils/logger';
+import morgan from '@middlewares/morgan';
 import koaConditionalGet from 'koa-conditional-get';
-
-morgan.format('custom-dev', customDevFormat);
 
 const app = new Koa();
 
-app.use(morgan('custom-dev'));
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(error);
-    ctx.body = { status: 'failed' };
-  }
-});
-
+app.use(morgan());
 app.use(koaConditionalGet());
 app.use(koaEtag());
-app.use(cors(corsConfig));
+app.use(cors());
 app.use(bodyParser());
 
 app.use(router.routes());
