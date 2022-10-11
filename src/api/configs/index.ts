8dee3d29d87c548c42094d50fdde5fba91a5a940
Retr0327 <philcoke35@gmail.com>
2022-10-11 17:33:06 +0800

    Remove corsConfig

--- src/api/configs/index.ts
+++ src/api/configs/index.ts
@@ -1,7 +1,5 @@
-import corsConfig from './cors';
-
 const isProduction = process.env.NODE_ENV === 'production';
 
 const PREFIX = isProduction ? '/api' : '/';
 
-export { corsConfig, PREFIX };
+export default PREFIX;
